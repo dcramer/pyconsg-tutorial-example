6bc602f0cdb75a281571feebb84c7078d5e359af
David Cramer <cramer@dropbox.com>
2014-06-11 12:11:21 -0700

    Basic API implementations

--- blog/api/post_details.py
+++ blog/api/post_details.py
@@ -2,10 +2,21 @@ from __future__ import absolute_import, division, unicode_literals
 
 from flask.ext.restful import Resource
 
+from blog.models import Post
+
 
 class PostDetailsResource(Resource):
     def get(self, post_id):
         """
         Return information about a given post.
         """
-        return {}, 200
+        post = Post.query.get(post_id)
+        if post is None:
+            return '', 404
+
+        return {
+            'id': post.id,
+            'title': post.title,
+            'body': post.body,
+            'pubDate': post.pub_date.isoformat(),
+        }
--- blog/api/post_index.py
+++ blog/api/post_index.py
@@ -1,11 +1,34 @@
 from __future__ import absolute_import, division, unicode_literals
 
-from flask.ext.restful import Resource
+from flask.ext.restful import reqparse, Resource
+
+from blog.models import Post
 
 
 class PostIndexResource(Resource):
-    def get(self, post_id):
+    get_parser = reqparse.RequestParser()
+    get_parser.add_argument('page', type=int, location='args')
+
+    def get(self):
         """
         Return a list of posts.
         """
-        return [], 200
+        args = self.get_parser.parse_args()
+
+        page = args.page or 0
+        limit = 10
+
+        start = page * limit
+        end = start + limit
+
+        post_list = Post.query.all()[start:end]
+
+        results = []
+        for post in post_list:
+            results.append({
+                'id': post.id,
+                'title': post.title,
+                'pubDate': post.pub_date.isoformat(),
+            })
+
+        return results, 200
